50e39d4744a5e566584eb39c324c74560cf0bea8
Gabe Wyatt <gwyattkelsey@gmail.com>
2014-04-27 09:49:21 -0700

    Added RSpec test for authorization as wrong user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user,  only: [:edit, :update]
+  before_action :correct_user,    only: [:edit, :update]
   def create
     @user = User.new(user_params)
     if @user.save
@@ -11,7 +12,7 @@ class UsersController < ApplicationController
     end
   end
   def edit
-    @user = User.find(params[:id])
+    
   end
   def new
     @user = User.new
@@ -20,7 +21,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -38,5 +38,10 @@ class UsersController < ApplicationController
     # Before filters
     def signed_in_user
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end  
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
