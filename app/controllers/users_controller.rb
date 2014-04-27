ae97969fefc10bab9a39cd14d123dea6087e4ee2
Gabe Wyatt <gwyattkelsey@gmail.com>
2014-04-27 08:53:11 -0700

    Added before_action :signed_in_user to :edit & :update actions of UserController to force a redirect to signin_url unless user is signed_in.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
-class UsersController < ApplicationController   
-   def create
+class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  def create
     @user = User.new(user_params)
     if @user.save
       sign_in @user
@@ -32,5 +33,10 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    
+    # Before filters
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end  
 end
