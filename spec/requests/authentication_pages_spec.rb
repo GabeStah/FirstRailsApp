ce75362c0e94033d125076cbb245cd1f3545a000
Gabe Wyatt <gwyattkelsey@gmail.com>
2014-04-28 00:39:11 -0700

    Added ruby-debug-ide gem.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,10 +15,13 @@ describe "Authentication" do
     before { visit signin_path }
 
     describe "with invalid information" do
+      let(:user) { FactoryGirl.create(:user) }      
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error') }
+      it { should_not have_link('Profile',     href: user_path(user)) }
+      it { should_not have_link('Settings',    href: edit_user_path(user)) }      
       
       describe "after visiting another page" do
         before { click_link "Home" }
@@ -51,12 +54,7 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user) }
 
       describe "when attempting to visit a protected page" do
-        before do
-          visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
-        end
+        before { sign_in user }
 
         describe "after signing in" do
 
